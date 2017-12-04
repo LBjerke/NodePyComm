de28684192f8821e5aea6968060636700ca1d2b0
Loyde Cochran-Bjerke <loyde.b.bjerke@gmail.com>
2017-12-03 18:04:51 -0700

    slight update

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ var pythonScript = spawn('python',['./pythonScripts/test.py']);
 var data = [3,2,3,4,5,6,7,8,9];//this is just the data you want to send in to the python script
 var dataString = ''; //the data String that is read back
 
+//reads data from python standard out
 pythonScript.stdout.on('data', function(data){
   dataString += data.toString();
 });
 
-
+//when the end of the standard out
 pythonScript.stdout.on('end', function(){
   console.log(dataString);
-  var spliter = dataString.split(',');
+  var splitter = dataString.split(',');
   //for UDP send
-  UDPsender.UDPsend(spliter[0],spliter[1],dataString);
+  UDPsender.UDPsend(splitter[0],splitter[1],dataString);
   //for TCP send
-  TCPsender.TCPsend(spliter[0],spliter[1],dataString);
+  TCPsender.TCPsend(splitter[0],splitter[1],dataString);
 });
 //this is for sending data in to python Script
 pythonScript.stdin.write(JSON.stringify(data));
